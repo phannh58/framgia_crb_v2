3058c0f9c5e651849baf39fdadce78149ea1a016
1312071 <1312071@student.hcmus.edu.vn>
2017-07-03 13:18:26 +0700

    test login facebook and google

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,26 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
   config.shared_context_metadata_behavior = :apply_to_host_groups
+  OmniAuth.config.add_mock(:facebook,
+    {provider: :facebook,
+    uid: "1234567",
+    info: {
+      first_name: "Ng",
+      name: "test",
+      email: "test@gmail.com"},
+    credentials: {
+      expires_at: Time.now + 1.week,
+      token: "1234567"
+    }})
+  OmniAuth.config.add_mock(:google_oauth2,
+    {provider: :google_oauth2,
+    uid: "1234567",
+    info: {
+      first_name: "Ng",
+      name: "test",
+      email: "test@gmail.com"},
+    credentials: {
+      expires_at: Time.now + 1.week,
+      token: "1234567"
+    }})
 end
